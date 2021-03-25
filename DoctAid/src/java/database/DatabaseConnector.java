a57cf721d46ca19dfc125ec37d29314c67ce7c0f
SanjulaD <desanjula99@gmail.com>
2021-03-25 10:02:57 +0530

    sass properties changes

--- DoctAid/src/java/database/DatabaseConnector.java
+++ DoctAid/src/java/database/DatabaseConnector.java
@@ -4,18 +4,18 @@ import java.sql.*;
 
 public class DatabaseConnector {
 
-	public static Connection getDatabase() {
+    public static Connection getDatabase() {
 
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/doctaid?autoReconnect=true&useSSL=false", "root", "");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/doctaid?autoReconnect=true&useSSL=false", "root", "");
 
-			return con;
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
 }
